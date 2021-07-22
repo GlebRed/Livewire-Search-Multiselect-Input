1748e4534b6ef0ca8d01b97526efbd222889b967
GlebRed <glebredko@gmail.com>
2021-07-22 19:26:30 +0100

    Minor fix

--- src/SearchMultiselectInputServiceProvider.php
+++ src/SearchMultiselectInputServiceProvider.php
@@ -14,6 +14,8 @@ class SearchMultiselectInputServiceProvider extends ServiceProvider
   public function boot()
   {
 
+    $this->loadViewsFrom(__DIR__.'/../resources/views', 'search-multiselect-input');
+
     if ($this->app->runningInConsole()) {
       // Publish views
       $this->publishes([
